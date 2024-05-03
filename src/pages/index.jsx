d1c889f6f01fd4c3db36d1b95de17d19c7dbc32d
jay-p-b-7span <jay.p.b@7span.com>
2024-05-03 23:10:42 +0530

    Updated with global metadata load

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,24 @@
+import directus from "../utils/directus";
+import { readSingleton } from "@directus/sdk";
+import { useState } from "preact/compat";
+
 
 export function Home(){
+    const [global, setGlobal ] = useState( {});
+    const fetchData =  async () => {
+
+        const global = await directus.request(readSingleton("global"));
+
+        setGlobal(global)
+
+
+    };
+    fetchData()
+
     return (
         <div>
             Home Page
+            <h1>{global.title}</h1>
         </div>
     )
 }
